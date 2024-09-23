c3fb6e2a9344e2c3d8ad354e7bdc45f764f518b6
terrnitllc <terrnitllc.work@gmail.com>
2024-09-23 23:37:34 +0500

    rss tweaks for vercel

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,29 +1,29 @@
-import rss from "@astrojs/rss";
-import { getPostInfoList } from "@lib/mdx";
+import rss from '@astrojs/rss'
+import { getPostInfoList } from '@lib/mdx'
 
-import { SITE } from "@consts";
+import { SITE } from '@consts'
 
 type Context = {
   site: string
 }
 
 export async function GET(context: Context) {
-	const articles = await getPostInfoList("en")
-//   const projects = (await getCollection("projects"))
-//     .filter(project => !project.data.draft);
+  const articles = await getPostInfoList('en')
 
   const items = [...articles]
-    .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
 
   return rss({
     title: SITE.TITLE,
     description: SITE.DESCRIPTION,
     site: context.site,
     items: items.map((item) => ({
-      title: item.data.title,
-      description: item.data.description,
-      pubDate: item.data.date,
-      link: `/${item.collection}/${item.slug}/`,
+      title: item.title,
+      description: item.description,
+      pubDate: item.date,
+      link: item.href,
+	  author: item.author
+      // TODO: Specify source
+      // source: item.source
     })),
-  });
+  })
 }
