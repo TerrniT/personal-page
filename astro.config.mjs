52d81d666ebe65f9bce99e15a31dde74c607b413
terrnitllc <terrnitllc.work@gmail.com>
2024-09-28 00:42:04 +0500

    added vercel server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from 'astro-icon'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
 import react from '@astrojs/react'
-import svelte from '@astrojs/svelte';
+import svelte from '@astrojs/svelte'
 
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import remarkToc from 'remark-toc'
@@ -14,51 +14,60 @@ import rehypeSlug from 'rehype-slug'
 import remarkBreaks from 'remark-breaks'
 import remarkGfm from 'remark-gfm'
 
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: 'https://terrnit.com',
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), vue(), react(), svelte(),  icon(), mdx({
-    //   syntaxHighlight: false,
-    shikiConfig: {
-      theme: 'solarized-dark',
-      themes: {
-        light: 'solarized-light',
-        dark: 'solarized-dark',
-        'dark-green': 'catppuccin-macchiato'
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    vue(),
+    react(),
+    svelte(),
+    icon(),
+    mdx({
+      //   syntaxHighlight: false,
+      shikiConfig: {
+        theme: 'solarized-dark',
+        themes: {
+          light: 'solarized-light',
+          dark: 'solarized-dark',
+          'dark-green': 'catppuccin-macchiato',
+        },
+
+        defaultColor: false,
+        cssVariablePrefix: '--shiki-',
+
+        wrap: true,
       },
-
-      defaultColor: false,
-      cssVariablePrefix: '--shiki-',
-
-      wrap: true,
-    },
-	//   { theme: "css-variables" }
-    remarkPlugins: [remarkGfm, remarkBreaks, remarkToc],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: 'wrap',
-          properties: {
-            className: ['anchor'],
+      //   { theme: "css-variables" }
+      remarkPlugins: [remarkGfm, remarkBreaks, remarkToc],
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: 'wrap',
+            properties: {
+              className: ['anchor'],
+            },
           },
-        },
-      ],
-      [
-        rehypeExternalLinks,
-        {
-          properties: {
-            class: 'external-link',
+        ],
+        [
+          rehypeExternalLinks,
+          {
+            properties: {
+              class: 'external-link',
+            },
+            target: '_blank',
+            rel: ['noopener noreferrer'],
           },
-          target: '_blank',
-          rel: ['noopener noreferrer'],
-        },
+        ],
       ],
-    ],
-  })],
+    }),
+  ],
+  adapter: vercel()
 })
